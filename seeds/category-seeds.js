2bafa8979e075c863b6ecad5fe1d7527b734b4e9
Wumbo-dot <94250514+Wumbo-dot@users.noreply.github.com>
2022-08-06 12:11:05 -0500

    updated category-seed with computer product names

--- seeds/category-seeds.js
+++ seeds/category-seeds.js
@@ -1,1 +1,30 @@
 const { Category } = require('../models');
+const categoryData =  [
+    {
+        category_name: 'GPUs'
+    },
+    {
+        category_name: 'CPUs'
+    },
+    {
+        category_name: 'Power Supply Units'
+    },
+    {
+        category_name: 'Mother Boards'
+    },
+    {
+        category_name: 'Memory'
+    },
+    {
+        category_name: 'Storage'
+    },
+    {
+        category_name: 'Computer Cases'
+    },
+    {
+        category_name: 'CPU Coolers'
+    },
+    {
+        category_name: 'Peripherals'
+    },
+]
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3013;
 
+app.engine('handlebars', hbs.engine);
+app.search('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(_dirname, 'public')));
 
 app.use(routes);
 
