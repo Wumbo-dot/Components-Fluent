ca5aceb0ab6953df9f611c9329a53f39545f8f82
Halima-Ikbal <100046854+Halima-Ikbal@users.noreply.github.com>
2022-08-10 15:07:17 -0500

    helper.js and auth.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,4 @@
 module.exports = {
-  format_date: date => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-      date
-    ).getFullYear()}`;
-  },
   format_url: url => {
     return url
       .replace('http://', '')
@@ -12,11 +7,5 @@ module.exports = {
       .split('/')[0]
       .split('?')[0];
   },
-  format_plural: (word, amount) => {
-    if (amount !== 1) {
-      return `${word}s`;
-    }
-
-    return word;
-  }
+  
 };
